712e8350a509c3a1bf4b9b2f7f640914d38fad61
Stephan Ude <ude@mediatis.de>
2023-08-09 19:49:48 +0200

    [TASK] configuration document version handling

---
+++ Classes/Registry/EventListener/CoreRegistryUpdateEventListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DigitalMarketingFramework\Typo3\Distributor\JsonRequest\Registry\EventListener;
+
+use DigitalMarketingFramework\Distributor\JsonRequest\DistributorJsonRequestInitialization;
+use DigitalMarketingFramework\Typo3\Core\Registry\EventListener\AbstractCoreRegistryUpdateEventListener;
+
+class CoreRegistryUpdateEventListener extends AbstractCoreRegistryUpdateEventListener
+{
+    public function __construct()
+    {
+        parent::__construct(new DistributorJsonRequestInitialization());
+    }
+}
