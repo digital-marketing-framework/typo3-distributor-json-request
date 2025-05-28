9638f4e2258d47a72273ab2eaae17df962bb330c
s-ude <ude@mediatis.de>
2025-05-28 09:40:19 +0200

    [TASK] TYPO3 13 Update (MFR-136) (#4)

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Digital Marketing Framework - Distributor - HTTP JSON Request',
+    'title' => 'Anyrel - Distributor - HTTP JSON Request',
     'description' => 'Send form data to different target systems via HTTP JSON requests',
     'category' => 'be',
     'author_email' => 'info@mediatis.de',
     'author_company' => 'Mediatis AG',
     'state' => 'stable',
-    'version' => '2.1.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
-            'dmf_distributor_core' => '2.0.0-2.99.99',
+            'typo3' => '12.4.0-13.4.99',
+            'dmf_distributor_core' => '3.0.0-3.99.99',
         ],
         'conflicts' => [
         ],
